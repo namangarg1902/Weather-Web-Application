03804a9ef7a078e6f28fb04f3fd83f4d7d73f110
Naman <namangarg490@gmail.com>
2022-10-15 22:55:15 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+let weather = {
+    "apiKey": "690b4c1a1034f6af67ffc357cba277cd",
+    fetchWeather: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+             + city
+             + "&units=metric&appid="
+             + this.apiKey 
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather : function(data , error){
+        const { name } = data ;
+        const {icon , description} = data.weather[0];
+        const {temp, humidity} = data.main ;
+        const {speed } = data.wind;
+        console.log(name , icon , description , temp , humidity , speed );
+        document.querySelector(".city").innerText = "Weather in " + name  ;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText ="Humidity : " +  humidity + "%";
+        document.querySelector(".wind").innerText ="Wind Speed : " + speed + "km/hr";
+        // document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name +"')" ;
+        // if(error){
+        //     
+        // }
+
+    },
+    search : function(){
+        try {
+            this.fetchWeather(document.querySelector('.search-bar').value);
+        } catch (error) {
+            alert("Enter applicable city");
+        }
+    }
+}
+
+document.querySelector('.search-button').addEventListener("click", function(){
+    weather.search();
+})
+
+document.querySelector('.search-bar').addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+})
